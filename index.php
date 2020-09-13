674297ee21b37dcb9e7051cba1b818c42233a4df
Robert Ochoa <ochoa.robert1@gmail.com>
2020-09-13 18:41:32 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>d1 limited</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+        <link rel="manifest" href="manifest.json">
+        <meta name="msapplication-TileColor" content="#FFFFFF">
+        <meta name="msapplication-TileImage" content="ms-icon-144x144.png">
+        <meta name="theme-color" content="#FFFFFF">
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+        <style type="text/css">
+            body {
+                font-family: 'Titillium Web', sans-serif;
+                background: #FFFFFF;
+            }
+
+            .main-container {
+                display: -webkit-box;
+                display: -moz-box;
+                display: -webkit-flex;
+                display: -ms-flexbox;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-flow: column;
+                min-height: 97vh;
+            }
+
+            .main-container-logo img {
+                max-width: 500px;
+                margin-bottom: 8rem;
+            }
+
+            h1 {
+                display: none;
+            }
+
+            h2 {
+                align-self: flex-end;
+                font-size: 1.2rem;
+                font-weight: 300;
+            }
+
+            @media (max-width: 1170px) {
+                .main-container-logo img {
+                    max-width: 400px;
+                    margin-bottom: 5rem;
+                }
+            }
+
+            @media (max-width: 480px) {
+                .main-container-logo img {
+                    max-width: 300px;
+                    margin-bottom: 8rem;
+                }
+            }
+
+            @media (max-width: 360px) {
+                h2 {
+                    align-self: flex-end;
+                    font-size: 1.1rem;
+                    font-weight: 300;
+                }
+
+                .main-container-logo img {
+                    max-width: 250px;
+                    margin-bottom: 8rem;
+                }
+            }
+
+        </style>
+    </head>
+
+    <body>
+        <div class="main-container">
+            <div class="main-container-logo">
+                <img src="logo.png" alt="d1 limited" />
+            </div>
+            <div class="main-container-text">
+                <h1>d1 limited</h1>
+                <h2>TIN1000004738186 | Barbados BB14004</h2>
+            </div>
+        </div>
+    </body>
+
+</html>
