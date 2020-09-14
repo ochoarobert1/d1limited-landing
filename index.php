6c829467b5f409a2c7debda7e9e3f4dd400d849d
Robert Ochoa <ochoa.robert1@gmail.com>
2020-09-13 20:14:16 -0400

    Landing maquetado - esperando Feedbacks

--- index.php
+++ index.php
@@ -1,104 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <title>d1 limited</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192" href="android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
-        <link rel="manifest" href="manifest.json">
-        <meta name="msapplication-TileColor" content="#FFFFFF">
-        <meta name="msapplication-TileImage" content="ms-icon-144x144.png">
-        <meta name="theme-color" content="#FFFFFF">
-        <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<head>
+    <meta charset="UTF-8">
+    <title>d1 limited</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="57x57" href="img/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="img/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="img/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+    <link rel="manifest" href="manifest.json">
+    <meta name="msapplication-TileColor" content="#FFFFFF">
+    <meta name="msapplication-TileImage" content="img/ms-icon-144x144.png">
+    <meta name="theme-color" content="#FFFFFF">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/fullpage.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
 
-        <style type="text/css">
-            body {
-                font-family: 'Titillium Web', sans-serif;
-                background: #FFFFFF;
-            }
-
-            .main-container {
-                display: -webkit-box;
-                display: -moz-box;
-                display: -webkit-flex;
-                display: -ms-flexbox;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                flex-flow: column;
-                min-height: 97vh;
-            }
-
-            .main-container-logo img {
-                max-width: 500px;
-                margin-bottom: 8rem;
-            }
-
-            h1 {
-                display: none;
-            }
-
-            h2 {
-                align-self: flex-end;
-                font-size: 1.2rem;
-                font-weight: 300;
-            }
-
-            @media (max-width: 1170px) {
-                .main-container-logo img {
-                    max-width: 400px;
-                    margin-bottom: 5rem;
-                }
-            }
-
-            @media (max-width: 480px) {
-                .main-container-logo img {
-                    max-width: 300px;
-                    margin-bottom: 8rem;
-                }
-            }
-
-            @media (max-width: 360px) {
-                h2 {
-                    align-self: flex-end;
-                    font-size: 1.1rem;
-                    font-weight: 300;
-                }
-
-                .main-container-logo img {
-                    max-width: 250px;
-                    margin-bottom: 8rem;
-                }
-            }
-
-        </style>
-    </head>
-
-    <body>
-        <div class="main-container">
-            <div class="main-container-logo">
-                <img src="logo.png" alt="d1 limited" />
-            </div>
-            <div class="main-container-text">
-                <h1>d1 limited</h1>
-                <h2>TIN1000004738186 | Barbados BB14004</h2>
+<body>
+    <div id="fullpage">
+        <div class="section">
+            <div class="section-wrapper container-fluid">
+                <div class="row">
+                    <div class="main-logo-container col-12">
+                        <img src="img/logo.png" alt="d1 limited" class="img-fluid" />
+                    </div>
+                    <div class="main-text-container col-12">
+                        <h2>TIN1000004738186 | Barbados BB14004</h2>
+                    </div>
+                </div>
             </div>
         </div>
-    </body>
+        <div class="section">
+            <div class="section-wrapper container-fluid">
+                <div class="row justify-content-center align-items-center">
+                    <div class="section-content col-6">
+                        <h1>d1 Limited it is a holding company registered under the Laws of Barbados. Since 2013, has developed and managed several investments in Dubai, Panamá, Spain, United States and Venezuela. Currently, it is focused in Dubai, United Arab Emirates and in David, Panamá.</h1>
+                    </div>
+                    <div class="section-content col-6">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1dpsXdyrju8F1Bz5zAe_FdPruOpHX5Geq" width="640" height="480" class="embed-responsive-item"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <div class="section-wrapper container">
+                <div class="row justify-content-center align-items-center">
+                    <div class="section-content section-logo col-3">
+                        <img src="img/bastian-logo.png" alt="Bastian Limited" class="img-fluid" />
+                    </div>
+                    <div class="section-content col-9">
+                        <h4>Dubai, United Arab Emirates</h4>
+                        <p>international procurement, which includes accommodations and airline ticketing as well as commercial supplies in and from Dubai, United Arab Emirates. This goods and services are provided and rendered by Bastián Limited1, a company created under the Laws of Ajman, United Arab Emirates, 1 with offices at JBC2 Tower,1806, Jumeirah Lakes Towers, Dubai.</p>
+                        <p>One recent example of this services by Bastián Limited, are aircraft charter services rendered to repatriate nationals worldwide during the COVID-19 pandemic.</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <div class="section-wrapper container">
+                <div class="row justify-content-center align-items-center">
+                    <div class="section-content section-logo col-3">
+                        <img src="img/quiteno-logo.png" alt="Bastian Limited" class="img-fluid" />
+                    </div>
+                    <div class="section-content col-9">
+                        <h4>David, Panamá</h4>
+                        <p>housing project (250/300 houses) to be developed in 24.7105 acres located in David, Chiriquí Province, Panamá and bought trough Promotora Quiteño Country S.A., a company created under the Laws of Panamá and fully owned by d1.</p>
+                        <p>Once developed, houses will be sold to the Housing Ministry in Panamá under the Fondo Solidario de Vivienda, which has a price of US$70,000.00 per house. Time frame it is estimated between eighteen to twenty four months and net income should be US$4,000,000. <a>https://www.miviot.gob.pa/archivos/37436</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/fullpage.min.js"></script>
+    <script type="text/javascript" src="js/functions.js"></script>
+</body>
 
 </html>
